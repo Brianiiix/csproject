f09ad5be5dd58e6c619b63c62f01ce2739e6274b
Brianiiix <brian890309@gmail.com>
2020-07-30 12:44:33 +0800

    commit2

--- routingBoard.cpp
+++ routingBoard.cpp
@@ -258,6 +258,7 @@ int main(int argc, const char * argv[]) {
     cfig.load_config("config/config_1_1.txt");
 
     string obs_name = "case/1.brd";
+        //wefewf
     cfig.load_config(obs_name+".obs");
 
     vector<Line_Box> outputedge;
